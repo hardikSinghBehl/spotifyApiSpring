6980d8fd99da5eff9f8951b95cbbb8b3edf705e2
Hardik Singh Behl <hardik.behl7444@gmail.com>
2021-06-12 22:19:14 +0530

    moving property for custom 404 to main class

--- src/main/java/com/hardik/pottify/PottifyApplication.java
+++ src/main/java/com/hardik/pottify/PottifyApplication.java
@@ -1,9 +1,12 @@
 package com.hardik.pottify;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = { ErrorMvcAutoConfiguration.class })
 public class PottifyApplication {
 
 	public static void main(String[] args) {
