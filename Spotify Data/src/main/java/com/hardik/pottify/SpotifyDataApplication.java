2e48e0140e2c3756610381e5777be8b26c309d28
Hardik Singh Behl <hardik.behl7444@gmail.com>
2021-06-12 21:46:10 +0530

    moving restTemplate bean to a bean class

--- Spotify Data/src/main/java/com/hardik/pottify/SpotifyDataApplication.java
+++ Spotify Data/src/main/java/com/hardik/pottify/SpotifyDataApplication.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class SpotifyDataApplication extends SpringBootServletInitializer{
+public class SpotifyDataApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpotifyDataApplication.class, args);
@@ -17,11 +15,6 @@ public class SpotifyDataApplication extends SpringBootServletInitializer{
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return super.configure(builder);
-	}
-	
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
 	}
 
 }
