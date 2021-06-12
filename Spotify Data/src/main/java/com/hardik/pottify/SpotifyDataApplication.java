10d97c64503cc122e0ca6c2398339402c54100a2
Hardik Singh Behl <hardik.behl7444@gmail.com>
2021-06-12 21:48:51 +0530

    removing springBootServletInitializer super class

--- Spotify Data/src/main/java/com/hardik/pottify/SpotifyDataApplication.java
+++ Spotify Data/src/main/java/com/hardik/pottify/SpotifyDataApplication.java
@@ -2,19 +2,12 @@ package com.hardik.pottify;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpotifyDataApplication extends SpringBootServletInitializer {
+public class SpotifyDataApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpotifyDataApplication.class, args);
-	}
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return super.configure(builder);
 	}
 
 }
