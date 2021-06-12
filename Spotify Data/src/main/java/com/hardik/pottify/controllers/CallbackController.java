eb568bcd1940f34b575001c5cf1a4d5054f1580c
Hardik Singh Behl <hardik.behl7444@gmail.com>
2021-06-12 21:51:58 +0530

    added additional libraries to project

--- Spotify Data/src/main/java/com/hardik/pottify/controllers/CallbackController.java
+++ Spotify Data/src/main/java/com/hardik/pottify/controllers/CallbackController.java
@@ -16,45 +16,43 @@ import com.hardik.pottify.service.UserDetails;
 
 @Controller
 public class CallbackController {
-	
+
 	@Autowired
 	private URL url;
-	
+
 	@Autowired
 	private AccessToken accessToken;
-	
+
 	@Autowired
 	private UserDetails userDetails;
-	
+
 	@Autowired
 	private CurrentPlaying currentPlaying;
-	
-	
+
 	@GetMapping("/callback")
-	public String handleCallback(@RequestParam(value = "code", required = false)String code,
-								 @RequestParam(value = "error", required = false)String error,
-								 Model model,HttpSession session) {
-		
-		if (error!=null) {
+	public String handleCallback(@RequestParam(value = "code", required = false) String code,
+			@RequestParam(value = "error", required = false) String error, Model model, HttpSession session) {
+
+		if (error != null) {
 			model.addAttribute("url", url.getAuthorizationURL());
 			return "callback-failure";
 		}
 		session.setAttribute("code", code);
 		String token = accessToken.getToken(code);
-		
+
 		session.setAttribute("accessToken", token);
-		model.addAttribute("accessToken",token);
+		model.addAttribute("accessToken", token);
 		System.out.println(token);
-		
+
 		model.addAttribute("userName", userDetails.getUsername(token));
-		
+
 		try {
 			model.addAttribute("currentPlaying", currentPlaying.getCurrentPlaying(token));
 			model.addAttribute("display", 1);
-		}catch(NoTrackPlayingException exception) {
+		} catch (NoTrackPlayingException exception) {
 			model.addAttribute("display", 0);
 		}
-		
+
 		return "callback-success";
 	}
 }
