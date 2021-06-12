309b59e1359614f0122c92095a75b9c011df6de4
Hardik Singh Behl <hardik.behl7444@gmail.com>
2021-06-12 21:49:50 +0530

    renaming main class

--- Spotify Data/src/main/java/com/hardik/pottify/PottifyApplication.java
+++ Spotify Data/src/main/java/com/hardik/pottify/PottifyApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpotifyDataApplication {
+public class PottifyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpotifyDataApplication.class, args);
+		SpringApplication.run(PottifyApplication.class, args);
 	}
 
 }
