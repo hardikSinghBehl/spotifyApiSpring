9eaaeadd2c47f62d3e52ab9b74f51de78547f8b3
Hardik Singh Behl <hardik.behl7444@gmail.com>
2021-06-12 21:44:42 +0530

    minor update: removing deprecated method due to version updgrde

--- Spotify Data/src/main/java/com/hardik/pottify/controllers/MyErrorController.java
+++ Spotify Data/src/main/java/com/hardik/pottify/controllers/MyErrorController.java
@@ -5,16 +5,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class MyErrorController implements ErrorController  {
- 
-		
-    @RequestMapping("/error")
-    public String handleError() {
-        return "error";
-    }
- 
-    @Override
-    public String getErrorPath() {
-        return null;
-    }
+public class MyErrorController implements ErrorController {
+
+	@RequestMapping("/error")
+	public String handleError() {
+		return "error";
+	}
+
 }
